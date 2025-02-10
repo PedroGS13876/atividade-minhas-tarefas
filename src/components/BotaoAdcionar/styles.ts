ceb3905233b25fa269678a7ae60c9f3196351033
Pedro <pedro_schunck@outlook.com.br>
2025-02-10 10:58:33 -0300

    salvando novas funçôes

---
+++ src/components/BotaoAdcionar/styles.ts
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const Circulo = styled(Link)`
+  heigh: 64px;
+  width: 64px
+  background-color: #44BD32;
+  color: #fff;
+  position: fixed;
+  bottom: 40px;
+  rigth: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  aling-items: center;
+  font-size: 40px;
+  text-decoration: none;
+`
